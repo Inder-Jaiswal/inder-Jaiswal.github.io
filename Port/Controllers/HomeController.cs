8d8e864e18b37ac992e8a4177f9bb1178854ecd4
Inder-Jaiswal <inderjaiswal00@yahoo.com>
2024-03-12 00:16:10 +0530

    edit for new layout with riya

---
+++ Port/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.ProjectModel;
+using Port.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using static Port.Models.DescriptionModel;
+
+namespace Port.Controllers
+{
+    public class HomeController : Controller
+    {
+        //{
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            DescriptionModel projectDescription = new DescriptionModel();
+            {
+                projectDescription.Name = "inder-PortFolio"; 
+                projectDescription.Id = "1st"; 
+                projectDescription.Description = "the site is built using angular.js , bootstrap, di, html, css, js and other technologies to support this data "; 
+                projectDescription.PArchitecture = "Angular on .net"; 
+            };
+        var listPD = new List<DescriptionModel>();
+            listPD.Add(projectDescription);
+        return View(listPD);
+    }
+
+    //public IActionResult Privacy()
+    //{
+    //    return View();
+    //}
+
+    //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    //public IActionResult Error()
+    //{
+    //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    //}
+    }
+}
---
+++ Port/Models/DescriptionModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Port.Models
+{
+    public class DescriptionModel
+    {
+       public string Id { get; set; } 
+       public string Name { get; set; }
+       public string Description { get; set; }
+       public string PArchitecture{ get; set; }
+       public string TechUsed { get; set; }
+        public  bool IsLive { get; set; }
+    }
+}
