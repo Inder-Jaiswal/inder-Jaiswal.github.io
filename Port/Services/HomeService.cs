8f8c93298199f22bb867e7f0381b35fd9bf3a05b
Inder-Jaiswal <inderjaiswal00@yahoo.com>
2024-03-12 08:21:41 +0530

    restored the html file to replace it further from react app

---
+++ Port/Services/HomeService.cs
@@ -0,0 +1,23 @@
+﻿using Port.Data.Entities;
+using Port.Data.Repository;
+
+namespace Port.Services
+{
+    public class HomeService: IHomeService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IHomeRepository _homeRepository;
+        #region ctor
+        public HomeService(IConfiguration configuration, IHomeRepository homeRepository)
+        {
+            this._configuration = configuration;
+            this._homeRepository = homeRepository;
+        }
+        #endregion
+
+        public List<Home>GetHomes()
+        {
+            return _homeRepository.GetHomeData();
+        }
+    }
+}
